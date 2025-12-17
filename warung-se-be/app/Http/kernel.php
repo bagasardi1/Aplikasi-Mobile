2109814a3582953ea253586fffe7178b04bc2b93
dodii11 <Dodikbima11@gmail.com>
2025-12-17 09:32:20 +0700

    Delete-Web

--- warung-se-be/app/Http/kernel.php
+++
@@ -1,8 +0,0 @@
-'api' => [
-    \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-    'throttle:api',
-    \Illuminate\Routing\Middleware\SubstituteBindings::class,
-],
-protected $routeMiddleware = [
-    'role' => \App\Http\Middleware\RoleMiddleware::class,
-];
